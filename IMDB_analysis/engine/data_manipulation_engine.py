5c9d61c0166e7395c0297827161733b9ec576d91
eldho541 <eldhobabym@gmail.com>
2019-12-03 17:19:50 +0530

    IMDB update

--- IMDB_analysis/engine/data_manipulation_engine.py
+++ IMDB_analysis/engine/data_manipulation_engine.py
@@ -9,15 +9,17 @@ def rating():
     ratingdata=sc.textFile(ratingdata)
     ## perform transformation on dataset
 
-    racollect = ratingdata.map(lambda a : a.split("::")[2])
-    racollect2 = racollect.map(lambda a : (a,1)).reduceByKey(lambda a,b : a+b).sortBy(lambda a : a[1],ascending=False)
+    racollect = ratingdata.map(lambda a : a.split("::")[1])
+    racollect2 = racollect.map(lambda a : (a, 1)).reduceByKey(lambda a,b : a+b).sortBy(lambda a : a[1],ascending=False)
     ## Create an Rdd for header
+
     head = sc.parallelize(["MovieId,No.views"])
-    print(head.take(1))
     totalview= head.union(racollect2)
-    print(totalview.getNumPartitions())
-## save result to a file as single partition using coalesce
-    totalview.coalesce(1).saveAsTextFile("C:\work\work-space\IMDB_analysis\Data\out121")
+    print(totalview.take(10))
+
+   # # save result to a file as single partition using coalesce
+   # #  totalview.coalesce(1).saveAsTextFile("C:\work\work-space\IMDB_analysis\Data\out121")
+
 rating()
 
 
