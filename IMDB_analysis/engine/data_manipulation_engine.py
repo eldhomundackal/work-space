34f631bdd1045c0426a1159571a99919ab9576b9
eldho541 <eldhobabym@gmail.com>
2019-12-09 11:13:22 +0530

    update Imdb

--- IMDB_analysis/engine/data_manipulation_engine.py
+++ IMDB_analysis/engine/data_manipulation_engine.py
@@ -27,13 +27,14 @@ def user():
     user_path = os.path.join("C:\work\work-space\IMDB_analysis\Data", "ml-1m", "users.dat")
     user_rdd = sc.textFile(user_path)
     print(user_rdd.take(5))
-# def process ():
-#     rating_rdd = rating()
-#     movie_rdd=movie()
-#     print(rating_rdd.take(5))
-#     # print(movie_rdd.take(5))
-#     m_watch_mov = rating_rdd.join(movie_rdd).map(lambda x: (x[1][1],x[1][0])).sortBy(lambda a : a[1],ascending=False).collect()
-#     for mov in m_watch_mov:
-#         print(mov)
-# process()
+def process ():
+    rating_rdd = rating()
+    movie_rdd=movie()
+    print(rating_rdd.take(5))
+    # print(movie_rdd.take(5))
+    m_watch_mov = rating_rdd.join(movie_rdd).map(lambda x: (x[1][1],x[1][0])).sortBy(lambda a : a[1],ascending=False).collect()
+    for mov in m_watch_mov:
+        print(mov)
+process()
 user()
+
